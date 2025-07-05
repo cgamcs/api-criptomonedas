a28f369c400bea00a6d8b08860e1474ba0154761
César García <cesar.garciamcs@outlook.com>
2025-07-05 12:50:44 -0600

    leyendo moneda y cripto seleccionada

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas')
+const monedaSelect = document.querySelector('#moneda')
+const formulario = document.querySelector('#formulario')
+
+const objtBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
 
 // Crear un Promise
 const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
@@ -7,6 +14,11 @@ const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptomonedas()
+
+    formulario.addEventListener('submit', submitFormulario)
+
+    criptomonedasSelect.addEventListener('change', leerValor)
+    monedaSelect.addEventListener('change', leerValor)
 })
 
 function consultarCriptomonedas() {
@@ -28,4 +40,29 @@ function selectCriptomonedas(criptomonedas) {
 
         criptomonedasSelect.appendChild(option)
     })
+}
+
+function leerValor(e) {
+    /*
+        e.target.name se refiere al name del select
+        es por eso que automaticamente puede insertar
+        los valores al objeto en la moneda o criptomoneda
+    */
+    objtBusqueda[e.target.name] = e.target.value
+}
+
+function submitFormulario(e) {
+    e.preventDefault()
+
+    // Validar
+    const { moneda, criptomoneda } = objtBusqueda
+
+    if(moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son necesarios')
+        return
+    }
+}
+
+function mostrarAlerta(mensaje) {
+
 }
